1f3a485540159979fc946f5f000d9823383fd2a7
Kamil <ninja59234@gmail.com>
2025-10-17 13:51:39 +0200

    Do ekranu motywacyjnego dodano 'Cytat dnia'

--- projekt-am/views/MotivationScreen.js
+++ projekt-am/views/MotivationScreen.js
@@ -4,14 +4,29 @@ import quotes from "../assets/quotes/quotes.json";
 
 export default function MotivationScreen() {
     const [quote, setQuote] = useState(null);
+    const [dailyQuote, setDailyQuote] = useState(null);
 
     const getRandomQuote = () => {
         const random = quotes[Math.floor(Math.random() * quotes.length)];
         setQuote(random);
     };
 
+    const getQuoteOfADay = () => {
+        const date = new Date();
+
+        const dayOfYear = Math.floor(
+            (date - new Date(date.getFullYear(), 0, 0)) / 86400000 // 86400000 to liczba ms w dniu
+        );
+
+        const index = dayOfYear % quotes.length;
+        const dailyQuote = quotes[index];
+
+        setDailyQuote(dailyQuote);
+    }
+
     useEffect(() => {
         getRandomQuote();
+        getQuoteOfADay();
     }, []);
 
     return (
@@ -33,9 +48,29 @@ export default function MotivationScreen() {
                 </Text>
             </Box>
 
-            <Button mt={10} onPress={getRandomQuote} colorScheme="blue">
+            <Button mt={10} onPress={getRandomQuote} colorScheme="blue" rounded="2xl">
                 Nowy cytat
             </Button>
+
+            <Box
+                position={"absolute"}
+                bottom={8}
+                p={1}
+                mb={8}
+                rounded="2xl"
+                shadow={3}
+                bg="gray.100"
+                w="100%"
+                maxW="90%"
+                alignItems="center"
+            >
+                <Text fontSize="lg" textAlign="center" bold>
+                    Cytat dnia:
+                </Text>
+                <Text fontSize="md" mt={2} italic textAlign="center">
+                    "{dailyQuote?.text || "Błąd w pobieraniu cytatu"}"
+                </Text>
+            </Box>
         </Center>
     );
 }
