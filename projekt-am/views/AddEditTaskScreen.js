8bc05790200479f5b8445d35cd01fe8829b3cad8
Kamil <ninja59234@gmail.com>
2025-11-11 15:43:03 +0100

    EditTaskScreen - działa teraz normalnie na IOS (klasa)

--- projekt-am/views/AddEditTaskScreen.js
+++ projekt-am/views/AddEditTaskScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Badge, Box, Button, HStack, Pressable, Text, VStack } from "native-base";
+import {Badge, Box, Button, HStack, Pressable, ScrollView, Text, VStack} from "native-base";
 import { TextInput, Platform } from "react-native";
 import axiosInstance from "../backend/axiosInstance";
 import { useAuth } from "../backend/context/AuthContext";
@@ -48,7 +48,6 @@ export default function AddEditTaskScreen({ route, navigation }) {
     };
 
     const onChangeDate = (event, selectedDate) => {
-        setShowDatePicker(false);
         if (selectedDate) {
             const formatted = selectedDate.toLocaleDateString("pl-PL");
             setDate(formatted);
@@ -85,98 +84,101 @@ export default function AddEditTaskScreen({ route, navigation }) {
     };
 
     return (
-        <Box flex={1} bg="white" p={5} pt={20}>
-            <VStack space={4}>
+        <ScrollView>
+            <Box flex={1} bg="white" p={5}>
+                <VStack space={4}>
 
-                <Text fontSize="2xl" bold>
-                    {editableTask ? "Edytuj zadanie" : "Dodaj zadanie"}
-                </Text>
+                    <Text fontSize="2xl" bold>
+                        {editableTask ? "Edytuj zadanie" : "Dodaj zadanie"}
+                    </Text>
 
-                <Text fontSize="md" bold>Nazwa:</Text>
-                <TextInput
-                    style={{
-                        borderWidth: 1,
-                        padding: 10,
-                        borderRadius: 10,
-                        height: 40
-                    }}
-                    placeholder="<Nazwa>"
-                    value={title}
-                    onChangeText={setTitle}
-                />
-
-                <Text fontSize="md" bold>Opis:</Text>
-                <TextInput
-                    style={{
-                        borderWidth: 1,
-                        padding: 10,
-                        borderRadius: 10,
-                        height: 120,
-                        textAlignVertical: "top"
-                    }}
-                    multiline
-                    placeholder="<Opis>"
-                    value={description}
-                    onChangeText={setDescription}
-                />
-
-                <Text fontSize="md" bold>Priorytet:</Text>
-                <HStack space={2}>
-                    {["Wysoki", "Średni", "Niski"].map(level => (
-                        <Button
-                            key={level}
-                            size="sm"
-                            colorScheme={priority === level ? "blue" : "gray"}
-                            onPress={() => setPriority(level)}
-                        >
-                            {level}
-                        </Button>
-                    ))}
-                </HStack>
-
-                <Text fontSize="md" bold>Termin:</Text>
-                <Button colorScheme="green" onPress={() => setShowDatePicker(true)}>
-                    {date ? `Wybrano: ${date}` : "Wybierz datę"}
-                </Button>
-                {showDatePicker && (
-                    <DateTimePicker
-                        value={new Date()}
-                        mode="date"
-                        display={Platform.OS === "ios" ? "spinner" : "calendar"}
-                        onChange={onChangeDate}
-                    />
-                )}
-
-                <Text fontSize="md" bold>Tagi:</Text>
-                <HStack space={2}>
+                    <Text fontSize="md" bold>Nazwa:</Text>
                     <TextInput
                         style={{
-                            flex: 1,
                             borderWidth: 1,
                             padding: 10,
-                            borderRadius: 10 }}
-                        placeholder="<Tag>"
-                        value={tagInput}
-                        onChangeText={setTagInput}
+                            borderRadius: 10,
+                            height: 40
+                        }}
+                        placeholder="<Nazwa>"
+                        value={title}
+                        onChangeText={setTitle}
                     />
-                    <Button onPress={handleAddTag}>Dodaj</Button>
-                </HStack>
 
-                <HStack space={2} flexWrap="wrap" mt={2}>
-                    {tags.map(tag => (
-                        <Pressable key={tag} onPress={() => removeTag(tag)}>
-                            <Badge colorScheme="purple" mr={1} mb={1}>
-                                {tag} X
-                            </Badge>
-                        </Pressable>
-                    ))}
-                </HStack>
+                    <Text fontSize="md" bold>Opis:</Text>
+                    <TextInput
+                        style={{
+                            borderWidth: 1,
+                            padding: 10,
+                            borderRadius: 10,
+                            height: 120,
+                            textAlignVertical: "top"
+                        }}
+                        multiline
+                        placeholder="<Opis>"
+                        value={description}
+                        onChangeText={setDescription}
+                    />
 
-                <Button mt={5} colorScheme="blue" onPress={handleSave}>
-                    {editableTask ? "Zapisz zmiany" : "Dodaj zadanie"}
-                </Button>
+                    <Text fontSize="md" bold>Priorytet:</Text>
+                    <HStack space={2}>
+                        {["Wysoki", "Średni", "Niski"].map(level => (
+                            <Button
+                                key={level}
+                                size="sm"
+                                colorScheme={priority === level ? "blue" : "gray"}
+                                onPress={() => setPriority(level)}
+                            >
+                                {level}
+                            </Button>
+                        ))}
+                    </HStack>
 
-            </VStack>
-        </Box>
+                    <Text fontSize="md" bold>Termin:</Text>
+                    <Button colorScheme="green" onPress={() => setShowDatePicker(!showDatePicker)}>
+                        {date ? `Wybrano: ${date}` : "Wybierz datę"}
+                    </Button>
+                    {showDatePicker && (
+                        <DateTimePicker
+                            value={new Date()}
+                            mode="date"
+                            display={Platform.OS === "ios" ? "spinner" : "calendar"}
+                            textColor={"black"}
+                            onChange={onChangeDate}
+                        />
+                    )}
+
+                    <Text fontSize="md" bold>Tagi:</Text>
+                    <HStack space={2}>
+                        <TextInput
+                            style={{
+                                flex: 1,
+                                borderWidth: 1,
+                                padding: 10,
+                                borderRadius: 10 }}
+                            placeholder="<Tag>"
+                            value={tagInput}
+                            onChangeText={setTagInput}
+                        />
+                        <Button onPress={handleAddTag}>Dodaj</Button>
+                    </HStack>
+
+                    <HStack space={2} flexWrap="wrap" mt={2}>
+                        {tags.map(tag => (
+                            <Pressable key={tag} onPress={() => removeTag(tag)}>
+                                <Badge colorScheme="purple" mr={1} mb={1}>
+                                    {tag} X
+                                </Badge>
+                            </Pressable>
+                        ))}
+                    </HStack>
+
+                    <Button mt={5} colorScheme="blue" onPress={handleSave}>
+                        {editableTask ? "Zapisz zmiany" : "Dodaj zadanie"}
+                    </Button>
+
+                </VStack>
+            </Box>
+        </ScrollView>
     );
 }
