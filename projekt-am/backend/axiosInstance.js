851a42e8e4d926ed8052403d1f45a077178018f7
Dorovvsky <dorofilip@wp.pl>
2025-11-11 16:25:13 +0100

    Można wreszcie zmienić email i hasło (jak żyć)

--- projekt-am/backend/axiosInstance.js
+++ projekt-am/backend/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://192.168.1.90:3001",
+    baseURL: "http://192.168.33.16:3001",
     timeout: 5000,
     headers: {
         "Content-Type": "application/json",
